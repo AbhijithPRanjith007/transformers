38f2db9332bc638a655a1c55f033175c8a5ead17
abhijith <abhijithpranjith007@gmail.com>
2021-09-26 12:42:42 +0000

    Firebase authentication setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './Component/Transformers_Login';
 import SignUp from './Component/Transformers_SignUp';
 import Home from './Component/Transformers_Home';
 function App() {
   return (
     <div className="App">
-     <Login/>
-     <SignUp/>
-     <Home/>
-   </div>
+        <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+    </Switch>
+  </Router>
+</div>
   );
 }
 
--- src/Component/Transformers_Login.js
+++ src/Component/Transformers_Login.js
@@ -1,5 +1,14 @@
 import "./Login.css";
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from "./firebase";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 function Login() {
+    const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const history = useHistory();
 function handleValidation(){
     // we need API call to validate user
     //we need to navigate home page 
@@ -9,8 +18,10 @@ function handleValidation(){
     <div className="Login">
       <h1>Login page </h1>
       <form onSubmit={handleValidation}>
-          <input className="text" type="email" placeholder="EmailID"/>
-          <input className="text" type="password" placeholder="Password"/>
+          <input className="text" type="email" placeholder="EmailID"  value={email}
+          onChange={(e) => setEmail(e.target.value)}/>
+          <input className="text" type="password" placeholder="Password" Value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
           <button  className="button" type="submit">Get Started</button>
           <a href="/SignUp">New User? Register here..</a>
       </form>
